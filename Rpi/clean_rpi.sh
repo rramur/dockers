5f9b24d90f088ba0e3ea2a1467abc77908905e4e
Raghu Ram <45059949+rramur@users.noreply.github.com>
2019-06-29 10:53:16 +0530

    Update clean_rpi.sh

--- Rpi/clean_rpi.sh
+++ Rpi/clean_rpi.sh
@@ -3,6 +3,14 @@ if [ $(docker ps -a | grep pi | wc -l) = "1" ]; then
     docker rm -f pi
 fi
 
+# Check the docker volume if it exists delete it
+if [ $(docker volume ls | grep Src | wc -l) = "1" ]; then
+    docker volume rm Src
+fi
+if [ $(docker volume ls | grep Run | wc -l) = "1" ]; then
+    docker volume rm Run
+fi
+
 # Check the docker image, if it exists delete it
 if [ $(docker images rpi:pi3 | grep rpi | wc -l) = "1" ]; then
     docker rmi -f rpi:pi3
