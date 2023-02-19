b2a9e3c2145805a20f18f1eaea5d754ed826dcaa
Raghuram Duddumpudi <raghu@Raghurams-Mac-Pro.local>
2023-02-19 15:52:10 +0530

    Added user defined src volume

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Docker Files
 
 This repository will have docker files for multiple platforms
+
+`sh dev.sh` will start the docker with src volume Dev-Src
+ `sh dev.sh <dir>` will start the docker with src volume as given dir
--- dev.sh
+++ dev.sh
@@ -1,3 +1,20 @@
+#!/bin/bash
+
+get_abs_filename() {
+  # $1 : relative filename
+  echo "$(cd "$(dirname "$1")" && pwd)/$(basename "$1")"
+}
+
+# Set the Enviroment variables 
+
+if [ -z "$1" ]
+  then
+    WORK_DIR=$PWD/Dev-Src
+  else
+    WORK_DIR=$(get_abs_filename $1)
+fi
+
+
 # Build the Docker if it doesnt exist
 if [ $(docker images ubuntu:dev | grep dev | wc -l) = "1" ]; then
     echo "Docker image dev exist"
@@ -9,5 +26,5 @@ fi
 if [ $(docker ps -a | grep dev | wc -l) = "1" ]; then
     docker exec -it dev bash
 else
-    docker run -it --rm -v $PWD/Dev-Src:/src --name dev ubuntu:dev bash
+    docker run -it --rm -v $WORK_DIR:/src --name dev ubuntu:dev bash
 fi
